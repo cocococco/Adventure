46bdb2f64aea842e2330d724e1692727e3ade438
cocococco <kyeonsoo26@gmail.com>
2020-03-22 22:18:28 +0900

    대충 이동

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Character : MonoBehaviour
 {
+    private Rigidbody2D ri;
+    public int speed;
+    public int jumpSpeed;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
-        
+        ri = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D))
+        {
+            ri.velocity = Vector2.right * speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            ri.velocity = Vector2.left * speed;
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            ri.AddForce(Vector2.up * jumpSpeed);
+        }
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            ri.velocity = Vector2.zero;
+        }
+        if (Input.GetKeyUp(KeyCode.A))
+        {
+            ri.velocity = Vector2.zero;
+        }
     }
 }
